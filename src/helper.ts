4396dd247dd8117f487299c63f1e7a5b41bca81c
MrDxD <nguyendoai7777@gmail.com>
2024-01-01 21:23:07 +0700

    app

--- src/helper.ts
+++
@@ -1,3 +0,0 @@
-export function $<T = HTMLDivElement>(selector: string) {return document.querySelector(selector) as T; }
-
-export function $$<T = HTMLDivElement>(selector: string) {return document.querySelectorAll(selector) as unknown as T[]; }
